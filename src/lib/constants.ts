e665c346af93f04fa61a4d75b29825f03ca48dcd
Sid-1819 <siddhesh.shirdhankar18@gmail.com>
2026-04-19 15:50:20 +0530

    Update GITHUB_REPO_URL to point to the correct GitHub repository

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const CHROME_WEB_STORE_URL =
   "https://chromewebstore.google.com/detail/tab-wise/ohpilcjcbejponkajcccllodpgcnnlpg";
-export const GITHUB_REPO_URL = "#";
+export const GITHUB_REPO_URL = "https://github.com/Sid-1819/tab-wise";
 export const PRODUCT_NAME = "Tab Wise";
 export const TAGLINE = "The wise way to wrangle your web.";
